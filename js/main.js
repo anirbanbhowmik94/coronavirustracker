5744d3c621e97750bb557c17746d8c018f017e61
anbhowmi <anbhowmi@ANBHOWMI-IN.oradev.oraclecorp.com>
2020-03-19 21:04:21 +0530

    Added Global Data

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-async function fetchList(){
+async function fetchCountry(){
   let url = 'https://coronavirus-19-api.herokuapp.com/countries/india';
   let response = await fetch(url);
-  let data = await response.json();
+  let countryData = await response.json();
   $("#splash").delay(2000).fadeOut();
-  renderList(data);
-  $("#app").delay(1000).fadeIn();
+  renderCountryData(countryData);
+  $("#country").delay(1000).fadeIn();
 }
 
-function renderList(data){
-  document.getElementById("app").innerHTML += `
+function renderCountryData(data){
+  document.getElementById("country").innerHTML += `
   <div class="row">
     <div class="col s12 m4">
       <div class="card center white">
@@ -31,7 +31,7 @@ function renderList(data){
     <div class="col s12 m4">
       <div class="card center white">
         <div class="card-content black-text">
-          <span class="card-title">Cases Today</span>
+          <span class="card-title">Reported Today</span>
           <hr>
           <p class="brown-text">${data.todayCases}</p>
         </div>
@@ -40,7 +40,7 @@ function renderList(data){
     <div class="col s12 m4">
       <div class="card center white">
         <div class="card-content black-text">
-          <span class="card-title">Total Deaths</span>
+          <span class="card-title">Deaths</span>
           <hr>
           <p class="grey-text">${data.deaths}</p>
         </div>
@@ -66,4 +66,45 @@ function renderList(data){
     </div>
   </div> 
   `
+}
+
+async function fetchGlobal(){
+  let url = 'https://coronavirus-19-api.herokuapp.com/all';
+  let response = await fetch(url);
+  let globalData = await response.json();
+  renderGlobalData(globalData);
+}
+
+function renderGlobalData(globalData){
+  document.getElementById("global").innerHTML += `
+  <div class="row">
+    <div class="col s12 m4">
+      <div class="card center white">
+        <div class="card-content black-text">
+          <span class="card-title">Cases</span>
+          <hr>
+          <p class="deep-purple-text">${globalData.cases}</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4">
+      <div class="card center white">
+        <div class="card-content black-text">
+          <span class="card-title">Deaths</span>
+          <hr>
+          <p class="grey-text">${globalData.deaths}</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4">
+      <div class="card center white">
+        <div class="card-content black-text">
+          <span class="card-title">Recovered</span>
+          <hr>
+          <p class="green-text">${globalData.recovered}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
 }
