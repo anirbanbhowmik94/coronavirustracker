0618c689b8b3a497ee2c03975e2393e31c7ce889
anbhowmi <anbhowmi@ANBHOWMI-IN.oradev.oraclecorp.com>
2020-03-20 22:38:15 +0530

    Added data dashboard for other affected countries

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-async function fetchCountry(){
-  let url = 'https://coronavirus-19-api.herokuapp.com/countries/india';
-  let response = await fetch(url);
-  let countryData = await response.json();
-  $("#splash").delay(2000).fadeOut();
-  renderCountryData(countryData);
-  $("#country").delay(1000).fadeIn();
+async function fetchData(){
+  let coutryDataCall = fetch('https://coronavirus-19-api.herokuapp.com/countries/india');
+  let globalDataCall = fetch('https://coronavirus-19-api.herokuapp.com/all');
+  let allCountriesDataCall = fetch('https://coronavirus-19-api.herokuapp.com/countries');
+  Promise.all([coutryDataCall,globalDataCall,allCountriesDataCall])
+    .then(dataset => Promise.all(dataset.map(data => data.json())))
+    .then(masterData => {
+      let indiaData = masterData[0];
+      let globalData = masterData[1];
+      let countryData = masterData[2];
+      $("#splash").delay(2000).fadeOut();
+      renderIndiaData(indiaData);
+      renderGlobalData(globalData);
+      countryData.forEach(function(data){
+        renderCountryData(data);
+      })
+      $("#country").delay(1000).fadeIn();
+    });
 }
 
-function renderCountryData(data){
+function renderIndiaData(indiaData){
   document.getElementById("country").innerHTML += `
   <div class="row">
     <div class="col s12 m4">
@@ -15,7 +26,7 @@ function renderCountryData(data){
         <div class="card-content black-text">
           <span class="card-title">Total Cases</span>
           <hr>
-          <p class="deep-purple-text">${data.cases}</p>
+          <p class="deep-purple-text">${indiaData.cases}</p>
         </div>
       </div>
     </div>
@@ -24,7 +35,7 @@ function renderCountryData(data){
         <div class="card-content black-text">
           <span class="card-title">Active Cases</span>
           <hr>
-          <p class="orange-text">${data.active}</p>
+          <p class="orange-text">${indiaData.active}</p>
         </div>
       </div>
     </div>
@@ -33,7 +44,7 @@ function renderCountryData(data){
         <div class="card-content black-text">
           <span class="card-title">Reported Today</span>
           <hr>
-          <p class="brown-text">${data.todayCases}</p>
+          <p class="brown-text">${indiaData.todayCases}</p>
         </div>
       </div>
     </div>
@@ -42,7 +53,7 @@ function renderCountryData(data){
         <div class="card-content black-text">
           <span class="card-title">Deaths</span>
           <hr>
-          <p class="grey-text">${data.deaths}</p>
+          <p class="grey-text">${indiaData.deaths}</p>
         </div>
       </div>
     </div>
@@ -51,7 +62,7 @@ function renderCountryData(data){
         <div class="card-content black-text">
           <span class="card-title">Critical</span>
           <hr>
-          <p class="red-text">${data.critical}</p>
+          <p class="red-text">${indiaData.critical}</p>
         </div>
       </div>
     </div>
@@ -60,21 +71,13 @@ function renderCountryData(data){
         <div class="card-content black-text">
           <span class="card-title">Recovered</span>
           <hr>
-          <p class="green-text">${data.recovered}</p>
+          <p class="green-text">${indiaData.recovered}</p>
         </div>
       </div>
     </div>
   </div> 
   `
 }
-
-async function fetchGlobal(){
-  let url = 'https://coronavirus-19-api.herokuapp.com/all';
-  let response = await fetch(url);
-  let globalData = await response.json();
-  renderGlobalData(globalData);
-}
-
 function renderGlobalData(globalData){
   document.getElementById("global").innerHTML += `
   <div class="row">
@@ -108,3 +111,31 @@ function renderGlobalData(globalData){
   </div>
   `;
 }
+function renderCountryData(countryData){ 
+  document.getElementById("countryList").innerHTML += `
+    <li class="collection-item avatar">
+      <i class="material-icons circle red">flag</i>
+      <span class="title">${countryData.country}</span>
+      <table class="striped centered">
+        <thead>
+          <tr>
+            <th>Total Cases</th>
+            <th>Active Cases</th>
+            <th>Reported Today</th>
+            <th>Deaths</th>
+            <th>Recovered</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="deep-purple-text">${countryData.cases}</td>
+            <td class="amber-text">${countryData.active}</td>
+            <td class="brown-text">${countryData.todayCases}</td>
+            <td class="grey-text">${countryData.deaths}</td>
+            <td class="green-text">${countryData.recovered}</td>
+          </tr>
+        </tbody>
+      </table>
+    </li>
+  `
+} 
